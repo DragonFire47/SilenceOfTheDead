f797543e99c5afe2c5294b0066a94dcfac286d55
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2022-03-01 22:57:51 -0800

    Add project files.

---
+++ SilenceOfTheDead/GUI.cs
@@ -0,0 +1,18 @@
+﻿using PulsarModLoader.CustomGUI;
+using static UnityEngine.GUILayout;
+
+namespace SilenceOfTheDead
+{
+    class GUI : ModSettingsMenu
+    {
+        public override string Name()
+        {
+            return "Silence Of The Dead";
+        }
+        public static bool ShouldBeMuting = true;
+        public override void Draw()
+        {
+            ShouldBeMuting = Toggle(ShouldBeMuting, "Should be muting the dead");
+        }
+    }
+}
---
+++ SilenceOfTheDead/ManagerPatch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using PulsarModLoader.Patches;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace SilenceOfTheDead
+{
+    [HarmonyPatch(typeof(PLPhotonVoice), "Update")]
+    class VoicePatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> targetSequence = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Ldloc_0),
+                new CodeInstruction(OpCodes.Ldfld),
+                new CodeInstruction(OpCodes.Ldc_I4_0),
+            };
+            List<CodeInstruction> injectionSequence = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Ldloc_0),
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(VoicePatch), "PatchMethod")),
+                new CodeInstruction(OpCodes.Ldc_I4_0),
+            };
+            return HarmonyHelpers.PatchBySequence(instructions, targetSequence, injectionSequence, HarmonyHelpers.PatchMode.REPLACE, HarmonyHelpers.CheckMode.NEVER);
+        }
+        static bool PatchMethod(PLPlayer PlayerForPhotonPlayer) //Is Player Muted?
+        {
+            if(PlayerForPhotonPlayer.GetPawn() == null || PLServer.Instance == null || PLServer.Instance.PlayerShipIsDestroyed)
+            {
+                return PlayerForPhotonPlayer.TS_IsMuted;
+            }
+            return PlayerForPhotonPlayer.TS_IsMuted || GUI.ShouldBeMuting && PlayerForPhotonPlayer.GetPawn().IsDead;
+        }
+    }
+}
